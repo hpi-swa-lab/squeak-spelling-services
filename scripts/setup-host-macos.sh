09319a5028ac0361a6d9d4a56f351343c4ea50fa
Christoph Thiede <38782922+LinqLover@users.noreply.github.com>
2021-06-21 22:48:37 +0200

    Remove debugging statements etc.

--- scripts/setup-host-macos.sh
+++ scripts/setup-host-macos.sh
@@ -2,5 +2,5 @@
 
 echo "Nothing to install, OSVM should contain SpellingPlugin"
 
-echo "Let's install ispell anyway so we can test mac OS callouts from IspellSpellingInterface, just in case the SpellingPlugin is not available ..."
+echo "But let's install ispell anyway so we can test mac OS callouts from IspellSpellingInterface, just in case the SpellingPlugin is not available ..."
 brew install ispell
