14e2e6d1fd4180043bd3f46f749f726ddb182f7f
Christoph Thiede <christoph.thiede@outlook.de>
2021-06-21 18:24:04 +0000

    Store absolute path in where-is-ispell.txt

--- scripts/setup-host-windows.sh
+++ scripts/setup-host-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone --depth 1 https://gitlab.com/vigou3/emacs-modified-windows.git
-echo emacs-modified-windows/aspell/bin/aspell.exe > ./where-is-ispell.txt
+realpath emacs-modified-windows/aspell/bin/aspell.exe > ./where-is-ispell.txt
 
 # DEBUG
 ls -la
