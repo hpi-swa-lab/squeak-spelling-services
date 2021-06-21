cc132af5665b5dd33fb35c8020d9bc3c7bb9c340
Christoph Thiede <christoph.thiede@outlook.de>
2021-06-21 17:22:47 +0000

    Fix typo

--- scripts/setup-host.sh
+++ scripts/setup-host.sh
@@ -4,6 +4,6 @@ set -e
 case "$(uname -s)" in
 	Linux*)			"$(dirname $0)/setup-host-linux.sh";;
 	Darwin*)		"$(dirname $0)/setup-host-macos.sh";;
-	CYGWIN|MINGW*)	"$(dirname $0)/setup-host-win.sh";;
+	CYGWIN|MINGW*)	"$(dirname $0)/setup-host-windows.sh";;
 	*)          	>&2 echo "WARNING: unknown host system";;
 esac
