9926a4496745d11daf41158f833704cc29eb35b8
Christoph Thiede <christoph.thiede@outlook.de>
2021-06-21 18:21:02 +0000

    No sudo on mac ¯\_(ツ)_/¯

--- scripts/setup-host-macos.sh
+++ scripts/setup-host-macos.sh
@@ -3,4 +3,4 @@
 echo "Nothing to install, OSVM should contain SpellingPlugin"
 
 echo "Let's install ispell anyway so we can test mac OS callouts from IspellSpellingInterface, just in case the SpellingPlugin is not available ..."
-sudo brew install ispell
+brew install ispell
