3ebe7c73ca9dd58674a4c4f1adc1b644f1287b14
Christoph Thiede <christoph.thiede@outlook.de>
2021-06-21 18:19:35 +0000

    CI: Install ispell for mac OS  too

--- scripts/setup-host-macos.sh
+++ scripts/setup-host-macos.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "Nothing to install, OSVM should contain SpellingPlugin"
+
+echo "Let's install ispell anyway so we can test mac OS callouts from IspellSpellingInterface, just in case the SpellingPlugin is not available ..."
+sudo brew install ispell
